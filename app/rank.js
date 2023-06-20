b9353a9d12099daeefdd4344b9a9ce210def07bc
Adrien Tremblay <adrien@adrientremblay.com>
2023-06-20 17:26:06 -0400

    Made things look nice

--- app/rank.js
+++ app/rank.js
@@ -2,7 +2,7 @@ export default function Rank(props) {
     return (
         <div class="rank">
             <h3>{props.name}</h3>
-            <span>{props.number} {props.difficulty} completed.</span>
+            <span>{props.number} {props.difficulty} <br/> completed.</span>
         </div>
     );
 }
