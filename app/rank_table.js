c314d0faf10a17df4e03f72da8f98d044389de75
Adrien Tremblay <adrien@adrientremblay.com>
2023-06-20 12:02:57 -0400

    progress

--- app/rank_table.js
+++ app/rank_table.js
@@ -1,3 +1,4 @@
+
 export default function RankTable() {
     return (
         <table className="rankTable">
@@ -7,8 +8,14 @@ export default function RankTable() {
                 <td className="content"></td>
             </tr>
             <tr>
-                <td></td>
-                <td></td>
+                <td>
+                    <div style={{position: "absolute", width: "50%", top: "50%", left: "50%"}} className="line horizontal-line"></div>
+                    <div style={{position: "absolute", height: "50%", top: "50%", left: "50%"}} className="line vertical-line"></div>
+                </td>
+                <td>
+                    <div className="line vertical-line"></div>
+                    <div className="overlay line horizontal-line"></div>
+                </td>
                 <td></td>
             </tr>
             <tr>
