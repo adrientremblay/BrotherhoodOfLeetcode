b0e601fd4af433db9b9bf36d6d5f9f827945873e
Adrien Tremblay <adrien@adrientremblay.com>
2023-06-20 17:04:24 -0400

    Complete the tree

--- app/rank_table.js
+++ app/rank_table.js
@@ -33,6 +33,61 @@ export default function RankTable() {
                     <td><div className="line vertical-line"></div></td>
                     <td><div className="line vertical-line"></div></td>
                 </tr>
+                <tr>
+                    <td className="content"><Rank name="Journeyman" number="30" difficulty="Easies"></Rank></td>
+                    <td className="content"><Rank name="Aspirant" number="24" difficulty="Mediums"></Rank></td>
+                    <td className="content"><Rank name="Champion" number="5" difficulty="Hards"></Rank></td>
+                </tr>
+                <tr>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                </tr>
+                <tr>
+                    <td className="content"><Rank name="Scribe" number="60" difficulty="Easies"></Rank></td>
+                    <td className="content"><Rank name="Warrior" number="36" difficulty="Mediums"></Rank></td>
+                    <td className="content"><Rank name="Legend" number="5" difficulty="Hards"></Rank></td>
+                </tr>
+                <tr>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                </tr>
+                <tr>
+                    <td className="content"><Rank name="Senior Scribe" number="60" difficulty="Easies"></Rank></td>
+                    <td className="content"><Rank name="Lancer" number="36" difficulty="Mediums"></Rank></td>
+                    <td className="content"><Rank name="Myth" number="5" difficulty="Hards"></Rank></td>
+                </tr>
+                <tr>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                </tr>
+                <tr>
+                    <td className="content"><Rank name="Chief Scribe" number="60" difficulty="Easies"></Rank></td>
+                    <td className="content"><Rank name="Knight" number="36" difficulty="Mediums"></Rank></td>
+                    <td className="content"><Rank name="Mage" number="5" difficulty="Hards"></Rank></td>
+                </tr>
+                <tr>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                </tr>
+                <tr>
+                    <td className="content"><Rank name="Proctor" number="60" difficulty="Easies"></Rank></td>
+                    <td className="content"><Rank name="Paladin" number="36" difficulty="Mediums"></Rank></td>
+                    <td className="content"><Rank name="Wizard" number="5" difficulty="Hards"></Rank></td>
+                </tr>
+                <tr>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                    <td><div className="line vertical-line"></div></td>
+                </tr>
+                <tr>
+                    <td className="content"><Rank name="Loremaster" number="60" difficulty="Easies"></Rank></td>
+                    <td className="content"><Rank name="Sentinel" number="36" difficulty="Mediums"></Rank></td>
+                    <td className="content"><Rank name="Elder" number="5" difficulty="Hards"></Rank></td>
+                </tr>
             </tbody>
         </table>
     );
