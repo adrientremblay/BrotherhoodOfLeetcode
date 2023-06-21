5aca612bd250109ad30830f25ae74bf38870c61c
Adrien Tremblay <adrien@adrientremblay.com>
2023-06-21 08:37:43 -0400

    render users you pass into Ranks

--- app/rank.js
+++ app/rank.js
@@ -1,4 +1,12 @@
 export default function Rank(props) {
+    let members = props.users.map(user => {
+        return (
+            <div style={{width:"25px", height: "25px", border:"2px solid green"}}>
+                <img style={{width: "25px", height: "25px"}} src={user.imageUrl}></img>
+            </div>
+        );
+    });
+    
     return (
         <div className="rankContainer">
             <div className="rank">
@@ -6,7 +14,7 @@ export default function Rank(props) {
                 <span>{props.number} {props.difficulty} <br/> completed.</span>
             </div>
             <div className="orderMembers">
-
+                {members}
             </div>
         </div>
     );
